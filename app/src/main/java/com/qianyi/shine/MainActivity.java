c672e55dae9f6543cb3fe32fd48b99739965c297
xuejinzhu <985085485@qq.com>
2018-03-27 09:02:09 +0800

    不行就没招了

--- app/src/main/java/com/qianyi/shine/MainActivity.java
+++ app/src/main/java/com/qianyi/shine/MainActivity.java
@@ -1,10 +1,9 @@
 package com.qianyi.shine;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
