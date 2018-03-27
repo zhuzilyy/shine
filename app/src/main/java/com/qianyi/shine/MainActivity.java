4171316f2c7b53fecb1a3ae4f50bca57713e07e2
xuejinzhu <985085485@qq.com>
2018-03-27 10:10:46 +0800

    框架

--- app/src/main/java/com/qianyi/shine/MainActivity.java
+++ app/src/main/java/com/qianyi/shine/MainActivity.java
@@ -1,12 +1,25 @@
 package com.qianyi.shine;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import com.qianyi.shine.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void getResLayout() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
     }
 }
