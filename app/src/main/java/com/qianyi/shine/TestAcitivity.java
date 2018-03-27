37657e9c4026cec7b11b64f775fc07d63103dc60
xzy031424 <287368384@qq.com>
2018-03-27 09:56:09 +0800

    辛振宇测试提交

---
+++ app/src/main/java/com/qianyi/shine/TestAcitivity.java
@@ -0,0 +1,18 @@
+package com.qianyi.shine;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2018/3/27.
+ */
+
+public class TestAcitivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("ceshi ","456789");
+    }
+}
