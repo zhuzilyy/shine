3e310aa4c346fbcb14a8bd6a959204ff38cf7352
zhuzilyy <985085485@email.com>
2018-03-31 22:41:11 +0800

    购买vip界面

---
+++ app/src/main/java/com/qianyi/shine/ui/mine/activity/VipActivity.java
@@ -0,0 +1,49 @@
+package com.qianyi.shine.ui.mine.activity;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.qianyi.shine.R;
+import com.qianyi.shine.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * Created by Administrator on 2018/3/31.
+ */
+
+public class VipActivity extends BaseActivity {
+    @BindView(R.id.tv_title)
+    TextView tv_title;
+    @Override
+    protected void initViews() {
+        tv_title.setText("购买VIP");
+    }
+    @Override
+    protected void initData() {
+
+    }
+    @Override
+    protected void getResLayout() {
+        setContentView(R.layout.activity_vip);
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void setStatusBarColor() {
+
+    }
+    @OnClick({R.id.iv_back})
+    public void click(View view){
+        switch (view.getId()){
+            case R.id.iv_back:
+                finish();
+                break;
+        }
+    }
+}
