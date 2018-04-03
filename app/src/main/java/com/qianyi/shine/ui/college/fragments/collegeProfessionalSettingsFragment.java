b77f8b70dbacf41f1e2221e8ace6572268ba3fdc
xzy031424 <287368384@qq.com>
2018-04-03 18:28:12 +0800

    辛振宇2018.4.3 大学详情

--- app/src/main/java/com/qianyi/shine/ui/college/fragments/collegeProfessionalSettingsFragment.java
+++ app/src/main/java/com/qianyi/shine/ui/college/fragments/collegeProfessionalSettingsFragment.java
@@ -133,6 +133,7 @@ public class collegeProfessionalSettingsFragment extends BaseFragment {
                         mAdapter.setEnableLoadMore(true);
                         mSwipeRefreshLayout.setRefreshing(false);
 
+
                     }
                 });
             }
