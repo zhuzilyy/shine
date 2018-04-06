84bee1e801cefb33a0f8acc102e8afa58a345529
xzy031424 <28736834@qq.com>
2018-04-06 17:30:13 +0800

    辛振宇2018.4.5 专业统计图完成

--- app/src/main/java/com/qianyi/shine/ui/college/fragments/Profession_EstablishmentFragment.java
+++ app/src/main/java/com/qianyi/shine/ui/college/fragments/Profession_EstablishmentFragment.java
@@ -18,13 +18,19 @@ import android.widget.Toast;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.qianyi.shine.R;
+import com.qianyi.shine.api.apiHome;
 import com.qianyi.shine.base.BaseFragment;
 import com.qianyi.shine.fragment.adapter.GridAdapter;
 import com.qianyi.shine.fragment.adapter.PullToRefreshAdapter;
 import com.qianyi.shine.ui.account.activity.WebviewActivity;
+import com.qianyi.shine.ui.career_planning.entity.SuitableForMeEntity;
+import com.qianyi.shine.ui.college.activity.CollegeActivity;
+import com.qianyi.shine.ui.college.activity.ProfessionalActivity;
 import com.qianyi.shine.ui.college.adapter.AreaAdapter;
+import com.qianyi.shine.ui.college.adapter.EstablishAdapter;
 import com.qianyi.shine.ui.college.adapter.GirdDropDownAdapter;
 import com.qianyi.shine.ui.college.adapter.ListDropDownAdapter;
+import com.qianyi.shine.ui.college.adapter.ProfessionAdapter;
 import com.yyydjk.library.DropDownMenu;
 
 import java.util.ArrayList;
@@ -32,6 +38,8 @@ import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
+import okhttp3.Call;
+import okhttp3.Response;
 
 /**
  * Created by Administrator on 2018/4/3.
@@ -57,6 +65,10 @@ public class Profession_EstablishmentFragment extends BaseFragment {
     //**************
     private SwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView recyclerView;
+    private EstablishAdapter mAdapter;
+    public List<SuitableForMeEntity> list_temp;
+    private int mNextRequestPage = 1;
+    private static final int PAGE_SIZE = 6;
 
     @Override
     protected View getResLayout(LayoutInflater inflater, ViewGroup container) {
@@ -136,7 +148,7 @@ public class Profession_EstablishmentFragment extends BaseFragment {
         View contentView= LayoutInflater.from(getActivity()).inflate(R.layout.layout_refresh,null);
         swipeRefreshLayout=contentView.findViewById(R.id.swipeLayout);
         recyclerView= contentView.findViewById(R.id.rv_list);
-        initContentView(recyclerView,swipeRefreshLayout);
+        initContentView();
 
 
         //init dropdownview
@@ -148,32 +160,167 @@ public class Profession_EstablishmentFragment extends BaseFragment {
 
     /***
      * 处理加载的布局
-     * @param recyclerView
-     * @param swipeRefreshLayout
+     *
      */
-    private void initContentView(RecyclerView recyclerView, SwipeRefreshLayout swipeRefreshLayout) {
+    private void initContentView() {
         swipeRefreshLayout.setColorSchemeColors(Color.rgb(47, 223, 189));
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-//        //上拉加载
-//        initAdapter();
-//        //添加head
-//        addHeadView();
-//        //下拉刷新
-//        initRefreshLayout();
-//        mSwipeRefreshLayout.setRefreshing(true);
-//        refresh();
+        //上拉加载
+        initAdapter();
+
+        //下拉刷新
+        initRefreshLayout();
+        swipeRefreshLayout.setRefreshing(true);
+        refresh();
 
     }
 
     @Override
     protected void initData() {
+        list_temp=new ArrayList<>();
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+        list_temp.add(new SuitableForMeEntity("积分上飞机","斐林试剂佛我文件佛if我欧文覅积分","发链接非叫我佛教佛为我金佛我我飞机哦我IE见覅欧文"));
+
 
     }
 
     @Override
     protected void initListener() {
 
+    }
+
+    private void initAdapter() {
+        mAdapter = new EstablishAdapter(R.layout.establish_item,list_temp);
+        mAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
+            @Override
+            public void onLoadMoreRequested() {
+                loadMore();
+            }
+        });
+        mAdapter.openLoadAnimation(BaseQuickAdapter.SLIDEIN_LEFT);
+//        mAdapter.setPreLoadNumber(3);
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.addOnItemTouchListener(new OnItemClickListener() {
+            @Override
+            public void onSimpleItemClick(BaseQuickAdapter adapter, View view, int position) {
+                Toast.makeText(mActivity, "专业设置", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(getActivity(),CollegeActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+
+
+    }
+
+    private void initRefreshLayout() {
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refresh();
+            }
+        });
+    }
+    //刷新
+    private void refresh() {
+        Toast.makeText(mActivity, "1346458465", Toast.LENGTH_SHORT).show();
+        mNextRequestPage = 1;
+        mAdapter.setEnableLoadMore(false);//这里的作用是防止下拉刷新的时候还可以上拉加载
+        apiHome.refresh("http://www.baidu.com", mNextRequestPage, new com.qianyi.shine.callbcak.RequestCallBack<String>() {
+            @Override
+            public void onSuccess(Call call, Response response, String s) {
+                Log.i("ppp","131"+s);
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        setData(true,list_temp);
+                        mAdapter.setEnableLoadMore(true);
+                        swipeRefreshLayout.setRefreshing(false);
+
+
+                    }
+                });
+            }
+
+            @Override
+            public void onEror(Call call, int statusCode, Exception e) {
+                Log.i("ppp","132"+e);
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        setData(true,list_temp);
+                        mAdapter.setEnableLoadMore(true);
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                });
+
+
+            }
+        });
+
+    }
+    //加载
+    private void loadMore() {
+
+        apiHome.loadMore("http://www.baidu.com", mNextRequestPage, new com.qianyi.shine.callbcak.RequestCallBack<String>() {
+            @Override
+            public void onSuccess(Call call, Response response, String s) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        setData(false, list_temp);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onEror(Call call, int statusCode, Exception e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mAdapter.loadMoreFail();
+                        Toast.makeText(getActivity(), "网络错误", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        });
+    }
+
+    private void setData(boolean isRefresh, List data) {
+        mNextRequestPage++;
+        final int size = data == null ? 0 : data.size();
+        if (isRefresh) {
+            Log.i("uio","isisisojfsojf===()()()()()");
+            mAdapter.setNewData(data);
+        } else {
+            if (size > 0) {
+                mAdapter.addData(data);
+            }
+        }
+        if (size < PAGE_SIZE) {
+            //第一页如果不够一页就不显示没有更多数据布局
+            mAdapter.loadMoreEnd(isRefresh);
+            Toast.makeText(getActivity(), "第一页如果不够一页就不显示没有更多数据布局", Toast.LENGTH_SHORT).show();
+        } else {
+            mAdapter.loadMoreComplete();
+        }
     }
 
 
--- app/src/main/java/com/qianyi/shine/ui/college/fragments/Profession_ProspectsFragment.java
+++ app/src/main/java/com/qianyi/shine/ui/college/fragments/Profession_ProspectsFragment.java
@@ -1,17 +1,42 @@
 package com.qianyi.shine.ui.college.fragments;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.PercentFormatter;
 import com.qianyi.shine.R;
 import com.qianyi.shine.base.BaseFragment;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
 
 /**
  * Created by Administrator on 2018/4/3.
  */
 
 public class Profession_ProspectsFragment extends BaseFragment {
+    @BindView(R.id.line_chart)
+    public LineChart mLineChart;
+    @BindView(R.id.chart_01)
+    public PieChart chat01;
+    @BindView(R.id.chart_02)
+    public PieChart chat02;
+    @BindView(R.id.chart_03)
+    public PieChart chat03;
     @Override
     protected View getResLayout(LayoutInflater inflater, ViewGroup container) {
         return inflater.inflate(R.layout.fragment_profession_prospects,null);
@@ -19,6 +44,20 @@ public class Profession_ProspectsFragment extends BaseFragment {
 
     @Override
     protected void initViews() {
+        //折线图(毕业生平均月薪)
+        setLineChart(mLineChart);
+        loadLineChartData(mLineChart);
+        //饼图--就业方向
+        setPieChart(chat01);
+        loadPieChartData(chat01);
+        //饼图--行业去向
+        setPieChart(chat02);
+        loadPieChartData(chat02);
+        //饼图--地区方向
+        setPieChart(chat03);
+        loadPieChartData(chat03);
+
+
 
     }
 
@@ -31,4 +70,174 @@ public class Profession_ProspectsFragment extends BaseFragment {
     protected void initListener() {
 
     }
+
+
+    //**************折线统计图开始*******************************************
+    /**
+     * 设置折现图的样式
+     * @param chart
+     */
+    private void setLineChart(LineChart chart) {
+
+        chart.setDescription("Glan");
+        chart.setDrawGridBackground(false);//设置网格背景
+        chart.setScaleEnabled(false);//设置缩放
+        chart.setDoubleTapToZoomEnabled(false);//设置双击不进行缩放
+
+        //设置X轴
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);//设置X轴的位置
+//        xAxis.setTypeface(mTf);//设置字体
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawAxisLine(true);
+
+        //获得左侧侧坐标轴
+        YAxis leftAxis = chart.getAxisLeft();
+//        leftAxis.setTypeface(mTf);
+        leftAxis.setLabelCount(5);
+//        leftAxis.setAxisLineWidth(1.5f);
+
+        //设置右侧坐标轴
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setDrawAxisLine(false);//右侧坐标轴线
+        rightAxis.setDrawLabels(false);//右侧坐标轴数组Lable
+//        rightAxis.setTypeface(mTf);
+//        rightAxis.setLabelCount(5);
+//        rightAxis.setDrawGridLines(false);
+    }
+
+    /**
+     * 为折线图设置数据
+     * @param chart
+     */
+    private void loadLineChartData(LineChart chart){
+        //所有线的List
+        ArrayList<LineDataSet> allLinesList = new ArrayList<LineDataSet>();
+
+        ArrayList<Entry> entryList1 = new ArrayList<Entry>();
+        ArrayList<Entry> entryList2 = new ArrayList<Entry>();
+        for(int i=0;i<12;i++){
+            //Entry(yValue,xIndex);一个Entry表示一个点，第一个参数为y值，第二个为X轴List的角标
+            entryList1.add(new Entry((int)(Math.random()*65)+40,i));
+        }
+        for(int i=0;i<12;i++){
+            //Entry(yValue,xIndex);一个Entry表示一个点，第一个参数为y值，第二个为X轴List的角标
+            entryList2.add(new Entry((int)(Math.random()*3)+10,i));
+        }
+
+
+        //LineDataSet可以看做是一条线
+        LineDataSet dataSet1 = new LineDataSet(entryList1,"dataLine1");
+        dataSet1.setLineWidth(2.5f);
+        dataSet1.setCircleSize(4.5f);
+        dataSet1.setHighLightColor(Color.RED);//设置点击某个点时，横竖两条线的颜色
+        dataSet1.setDrawValues(false);//是否在点上绘制Value
+
+        LineDataSet dataSet2 = new LineDataSet(entryList2,"dataLine2");
+        dataSet1.setLineWidth(2.5f);
+        dataSet1.setCircleSize(4.5f);
+        dataSet1.setHighLightColor(Color.GREEN);//设置点击某个点时，横竖两条线的颜色
+        dataSet1.setDrawValues(false);//是否在点上绘制Value
+
+        allLinesList.add(dataSet1);
+        allLinesList.add(dataSet2);
+
+        //LineData表示一个LineChart的所有数据(即一个LineChart中所有折线的数据)
+        LineData mChartData = new LineData(getXAxisShowLable(),allLinesList);
+
+        // set data
+        chart.setData((LineData) mChartData);
+        chart.animateX(1500);//设置动画
+    }
+    //提供x轴数据
+    private ArrayList<String> getXAxisShowLable() {
+        ArrayList<String> m = new ArrayList<String>();
+        m.add("Jan");
+        m.add("Feb");
+        m.add("Mar");
+        m.add("Apr");
+        m.add("May");
+        m.add("Jun");
+        m.add("Jul");
+        m.add("Aug");
+        m.add("Sep");
+        m.add("Okt");
+        m.add("Nov");
+        m.add("Dec");
+        return m;
+    }
+//**************折线统计图结束*******************************************
+
+//**************饼图开始***************************************
+    /**
+     * 设置饼形图样式
+     * @param chart
+     */
+    private void setPieChart(PieChart chart) {
+        // apply styling
+        chart.setDescription("");
+        chart.setHoleRadius(52f);
+        chart.setTransparentCircleRadius(57f);
+        chart.setCenterText("MPChart\nAndroid");
+//        chart.setCenterTextTypeface(mTf);
+        chart.setCenterTextSize(18f);
+        chart.setUsePercentValues(true);
+
+        //中心文字颜色
+        chart.setCenterTextColor(Color.GREEN);
+    }
+
+    private void loadPieChartData(PieChart chart) {
+
+        ArrayList<Entry> entries = new ArrayList<Entry>();
+
+        for (int i = 0; i < 4; i++) {
+            entries.add(new Entry((int) (Math.random() * 70) + 30, i));
+        }
+
+        PieDataSet mPieDataSet = new PieDataSet(entries, "");
+
+        // space between slices
+        mPieDataSet.setSliceSpace(2f);
+        mPieDataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+
+        PieData mPieChartData = new PieData(getQuarters(),mPieDataSet);
+
+        // set data
+        chart.setData( mPieChartData);
+
+        //设置动画
+        chart.animateXY(900, 900);
+
+        Legend l = chart.getLegend();
+        l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
+
+        mPieChartData.setValueFormatter(new PercentFormatter());//设置显示成百分比
+//        mChartData.setValueTypeface(mTf);
+        mPieChartData.setValueTextSize(11f);//设置文字大小
+        mPieChartData.setValueTextColor(Color.WHITE);
+
+        //设置中心数据
+        chart.setCenterText("GlanWang");
+    }
+
+    /**
+     * 饼形图的划分
+     * @return
+     */
+    private ArrayList<String> getQuarters() {
+
+        ArrayList<String> q = new ArrayList<String>();
+        q.add("1st Quarter");
+        q.add("2nd Quarter");
+        q.add("3rd Quarter");
+        q.add("4th Quarter");
+
+        return q;
+    }
+
+
+
+//**************饼图结束***************************************
+
 }
