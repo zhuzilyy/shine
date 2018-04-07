80026b71237c5e9abedfcbc0d9f1c5d7053fda89
xzy031424 <28736834@qq.com>
2018-04-07 14:53:45 +0800

    辛振宇2018.4.5 职业详情

--- app/src/main/java/com/qianyi/shine/ui/home/activity/OccupationDetailActivity.java
+++ app/src/main/java/com/qianyi/shine/ui/home/activity/OccupationDetailActivity.java
@@ -1,15 +1,62 @@
 package com.qianyi.shine.ui.home.activity;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
 import com.qianyi.shine.R;
 import com.qianyi.shine.base.BaseActivity;
+import com.qianyi.shine.ui.college.adapter.MyPageAdapter;
+import com.qianyi.shine.ui.college.fragments.Profession_BaseInfoFragment;
+import com.qianyi.shine.ui.college.fragments.Profession_EstablishmentFragment;
+import com.qianyi.shine.ui.college.fragments.Profession_ProspectsFragment;
+import com.qianyi.shine.ui.gaokao_news.view.XTitleView;
+import com.qianyi.shine.ui.home.fragment.OccupationBaseInfoFragment;
+import com.qianyi.shine.ui.home.fragment.OccupationDetailsFragment;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
 
 /**
  * Created by Administrator on 2018/4/7.
  */
 
 public class OccupationDetailActivity extends BaseActivity {
+    @BindView(R.id.tablayout)
+    public TabLayout tab;
+    @BindView(R.id.view_pager)
+    public ViewPager viewPager;
+    @BindView(R.id.titile)
+    public XTitleView title;
+
+    public MyPageAdapter myPageAdapter;
+
     @Override
     protected void initViews() {
+        title.setBackListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        myPageAdapter = new MyPageAdapter(getSupportFragmentManager());
+        ArrayList<Fragment> datas = new ArrayList<Fragment>();
+        datas.add(new OccupationBaseInfoFragment());
+        datas.add(new OccupationDetailsFragment());
+        myPageAdapter.setData(datas);
+
+        ArrayList<String> titles = new ArrayList<String>();
+        titles.add("基本信息");
+        titles.add("职业细化");
+        myPageAdapter.setTitles(titles);
+
+        viewPager.setAdapter(myPageAdapter);
+        // 将ViewPager与TabLayout相关联
+        tab.setupWithViewPager(viewPager);
+
 
     }
 
