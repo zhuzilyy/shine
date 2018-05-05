d22f089b040d48ea8eaa0e926913d55b7c5079e7
zhuzilyy <985085485@email.com>
2018-05-05 21:33:33 +0800

    专业列表

--- app/src/main/java/com/qianyi/shine/ui/home/fragment/MajorBenkeFragment.java
+++ app/src/main/java/com/qianyi/shine/ui/home/fragment/MajorBenkeFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
@@ -26,11 +27,13 @@ import com.qianyi.shine.fragment.entity.SecondLevel;
 import com.qianyi.shine.fragment.entity.ThirdLevel;
 import com.qianyi.shine.ui.home.activity.PriorityProfessionalDetailsActivity;
 import com.qianyi.shine.ui.home.bean.Major;
+import com.qianyi.shine.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import okhttp3.Call;
 import okhttp3.Response;
 
@@ -41,6 +44,10 @@ import okhttp3.Response;
 public class MajorBenkeFragment extends BaseFragment {
     @BindView(R.id.rv)
     RecyclerView recyclerView;
+    @BindView(R.id.no_internet_rl)
+    RelativeLayout no_internet_rl;
+    @BindView(R.id.no_data_rl)
+    RelativeLayout no_data_rl;
     private View view_benke;
     private List<Major> majorList;
     private Intent intent;
@@ -63,101 +70,91 @@ public class MajorBenkeFragment extends BaseFragment {
     }
     @Override
     protected void initData() {
-        apiHome.getMajorList(apiConstant.MAJOR_LIST, "7", new RequestCallBack<String>() {
-            @Override
-            public void onSuccess(Call call, Response response, final String s) {
-                getActivity().runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Log.i("tag","111"+s);
-                        customLoadingDialog.dismiss();
-                        Gson gson=new Gson();
-                        MajorBean majorBean = gson.fromJson(s, MajorBean.class);
-                        List<FirstLevel> firstLevelList = majorBean.getData().getInfo().getMajorList();
-                        Log.i("tag",firstLevelList.size()+"======firstLevelList=========");
-                        //第一层数据
-                        for (int i = 0; i < firstLevelList.size(); i++) {
-                            String major_cate = firstLevelList.get(i).getMajor_cate();
-                            Major firstLevel=new Major("1","0",major_cate);
-                            majorList.add(firstLevel);
-                            //Log.i("tag","111"+major_cate);
-                            Toast.makeText(mActivity, ""+major_cate, Toast.LENGTH_SHORT).show();
-                            List<SecondLevel> secondList = firstLevelList.get(i).getMajor_second_list();
-                            for (int j = 0; j <secondList.size() ; j++) {
-                                //第二层数据
-                                String secondLevelName = secondList.get(j).getMajor_second_cate();
-                                String secondLevleId = secondList.get(j).getMajor_second_cate_id();
-                                Major secondLevel=new Major(secondLevleId,i+"",secondLevelName);
-                                majorList.add(secondLevel);
-                                Log.i("tag","222"+secondLevelName);
-                                Toast.makeText(mActivity, ""+secondLevelName, Toast.LENGTH_SHORT).show();
-                                List<ThirdLevel> thirdList = secondList.get(j).getMajor_list();
-                                for (int k = 0; k <thirdList.size() ; k++) {
-                                    String name = thirdList.get(k).getName();
-                                    String id = thirdList.get(k).getId();
-                                    Major thirdLevel=new Major(id,secondLevleId,name);
-                                    majorList.add(thirdLevel);
-                                    Log.i("tag","333"+name);
+        getData();
+    }
+    private void getData() {
+        //有网络
+        if (Utils.hasInternet()){
+            customLoadingDialog.show();
+            apiHome.getMajorList(apiConstant.MAJOR_LIST, "7", new RequestCallBack<String>() {
+                @Override
+                public void onSuccess(Call call, Response response, final String s) {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            recyclerView.setVisibility(View.VISIBLE);
+                            no_internet_rl.setVisibility(View.GONE);
+                            no_data_rl.setVisibility(View.GONE);
+                            Gson gson=new Gson();
+                            MajorBean majorBean = gson.fromJson(s, MajorBean.class);
+                            List<FirstLevel> firstLevelList = majorBean.getData().getInfo().getMajorList();
+                            if (firstLevelList.size()>0){
+                                //第一层数据
+                                for (int i = 0; i < firstLevelList.size(); i++) {
+                                    FirstLevel firstLevel = firstLevelList.get(i);
+                                    String major_cate =firstLevel .getMajor_cate();
+                                    String major_cateId=firstLevel.getMajor_cate_id();
+                                    long firstLongId=Long.parseLong(major_cateId);
+                                    Major firstMajor=new Major(firstLongId,0,major_cate);
+                                    majorList.add(firstMajor);
+                                    //Log.i("tag","111"+major_cate);
+                                    List<SecondLevel> secondList = firstLevel.getMajor_second_list();
+                                    for (int j = 0; j <secondList.size() ; j++) {
+                                        //第二层数据
+                                        SecondLevel secondLevel = secondList.get(j);
+                                        String secondLevelName = secondLevel.getMajor_second_cate();
+                                        String secondLevleId = secondLevel.getMajor_second_cate_id();
+                                        long secondLongId=Long.parseLong(secondLevleId);
+                                        Major secondMajor=new Major(secondLongId,firstLongId,secondLevelName);
+                                        majorList.add(secondMajor);
+                                        Log.i("tag","222"+secondLevelName);
+                                        List<ThirdLevel> thirdList = secondList.get(j).getMajor_list();
+                                        for (int k = 0; k <thirdList.size() ; k++) {
+                                            ThirdLevel thirdLevel = thirdList.get(k);
+                                            String name = thirdLevel.getName();
+                                            String id = thirdLevel.getId();
+                                            long thirdLongId=Long.parseLong(id);
+                                            Major thirdMajor=new Major(thirdLongId,secondLongId,name);
+                                            majorList.add(thirdMajor);
+                                            Log.i("tag","333"+name);
+                                        }
+                                    }
                                 }
+                            }else{
+                                recyclerView.setVisibility(View.GONE);
+                                no_internet_rl.setVisibility(View.GONE);
+                                no_data_rl.setVisibility(View.VISIBLE);
                             }
+                            //设置适配器
+                            setAdapter(majorList);
                         }
-                    }
-                });
-            }
-            @Override
-            public void onEror(Call call, int statusCode, Exception e) {
-                customLoadingDialog.dismiss();
-            }
-        });
-       /* Major A=new Major(1,0,"经济学");
-        Major B=new Major(2,0,"经济学");
-        Major C=new Major(3,0,"经济学");
-        Major D=new Major(4,0,"经济学");
-        Major E=new Major(5,0,"经济学");
-        list.add(A);
-        list.add(B);
-        list.add(C);
-        list.add(D);
-        list.add(E);
-        //第二层数据
-        for (int i = 6; i <7 ; i++) {
-            Major secondMajor=new Major(i,1,"工程经济学",1);
-            list.add(secondMajor);
+                    });
+                }
+                @Override
+                public void onEror(Call call, int statusCode, Exception e) {
+                    customLoadingDialog.dismiss();
+                    recyclerView.setVisibility(View.GONE);
+                    no_internet_rl.setVisibility(View.VISIBLE);
+                    no_data_rl.setVisibility(View.GONE);
+                }
+            });
+        }else{
+            recyclerView.setVisibility(View.GONE);
+            no_internet_rl.setVisibility(View.VISIBLE);
+            no_data_rl.setVisibility(View.GONE);
         }
-        //第三层数据
-        for (int i = 26; i <30 ; i++) {
-            Major thirdMajor=new Major(i,6,"土木工程经济学");
-            list.add(thirdMajor);
-        }
-        //第二层数据
-        for (int i = 10; i <14 ; i++) {
-            Major secondMajor=new Major(i,2,"工程经济学");
-            list.add(secondMajor);
-        }
-        //第二层数据
-        for (int i = 14; i <18 ; i++) {
-            Major secondMajor=new Major(i,3,"工程经济学");
-            list.add(secondMajor);
-        }
-        //第二层数据
-        for (int i = 18; i <22 ; i++) {
-            Major secondMajor=new Major(i,4,"工程经济学");
-            list.add(secondMajor);
-        }
-        //第二层数据
-        for (int i = 22; i <26 ; i++) {
-            Major secondMajor=new Major(i,5,"工程经济学");
-            list.add(secondMajor);
-        }*/
+    }
+
+    //加载适配器
+    private void setAdapter(List<Major> majorList) {
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         final TreeAdapter<Major> adapter = new TreeAdapter<>(getActivity());
         adapter.setNodes(majorList);
+        recyclerView.setAdapter(adapter);
+        customLoadingDialog.dismiss();
         adapter.setListener(new TreeItemClickListener() {
             @Override
             public void OnClick(Node node) {
-               /* if (node.getId() == 3) {
-                    adapter.addChildrenById(3, childs);
-                }*/
                 int level = node.getLevel();
                 if (level==2){
                     //startActivity(new Intent(getActivity(), ProfessionalActivity.class));
@@ -169,7 +166,7 @@ public class MajorBenkeFragment extends BaseFragment {
                             intent.setAction("com.action.setwilling");
                             intent.putExtra("majorName",node.getName());
                             getActivity().sendBroadcast(intent);
-                         //专业优先跳转的界面  或是  查专业跳转过来的界面
+                            //专业优先跳转的界面  或是  查专业跳转过来的界面
                         }else if(tag.equals("majorPriority") ||tag.equals("searchMajor")){
                             intent=new Intent(getActivity(), PriorityProfessionalDetailsActivity.class);
                             startActivity(intent);
@@ -178,10 +175,18 @@ public class MajorBenkeFragment extends BaseFragment {
                 }
             }
         });
-        recyclerView.setAdapter(adapter);
     }
+
     @Override
     protected void initListener() {
 
+    }
+    @OnClick({R.id.reload})
+    public void click(View view){
+        switch (view.getId()){
+            case R.id.reload:
+                getData();
+                break;
+        }
     }
 }
